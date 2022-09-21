db304738a8109b4570d40556a027d44f6c5f2171
esanolad <esanolad@gmail.com>
2022-09-21 15:09:59 +0400

    first Commit

---
+++ app/src/main/java/com/example/relativelayout/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.relativelayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        startActivity(new Intent(MainActivity.this,Google.class));
+    }
+}
